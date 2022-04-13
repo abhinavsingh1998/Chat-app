a48496c64beb25f174268d94172f1371a4b76cd7
Aftab <aftab.bhadki@ergos.in>
2022-04-13 12:21:45 +0530

    issue fixed

--- app/src/main/java/com/emproto/hoabl/feature/home/promisesUi/adapter/HoabelPromiseAdapter.kt
+++ app/src/main/java/com/emproto/hoabl/feature/home/promisesUi/adapter/HoabelPromiseAdapter.kt
@@ -5,63 +5,63 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.emproto.hoabl.R
 import com.emproto.hoabl.databinding.PromisesViewBinding
 import com.emproto.hoabl.feature.home.promisesUi.data.DataModel
 
 
-class HoabelPromiseAdapter(var context: Context) : RecyclerView.Adapter<HoabelPromiseAdapter.HoabelPromiseViewHolder>() {
-    private lateinit var onItemClickListener : View.OnClickListener
-    var dataList = emptyList<DataModel>()
-    internal fun setDataList(dataList: List<DataModel>) {
-        this.dataList = dataList
-    }
+class HoabelPromiseAdapter(
+    var context: Context,
+    val dataList: List<DataModel>,
+    val itemInterface: PromisedItemInterface
+) :
+    RecyclerView.Adapter<HoabelPromiseAdapter.HoabelPromiseViewHolder>() {
 
-    inner class HoabelPromiseViewHolder(var binding: PromisesViewBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HoabelPromiseViewHolder {
-        val view = PromisesViewBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val view = PromisesViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return HoabelPromiseViewHolder(view)
     }
+
     override fun onBindViewHolder(holder: HoabelPromiseViewHolder, position: Int) {
-        holder.binding.title.setOnClickListener(onItemClickListener)
-        holder.binding.fullviewHoabel.setOnClickListener(onItemClickListener)
+        val data = dataList[position]
+        holder.binding.title.text = data.title
+        holder.binding.desc.text = data.desc
+        holder.binding.image.setImageResource(data.image)
+        holder.binding.arrowImage.setImageResource(data.arrowImage)
+        holder.binding.itemCard.setOnClickListener {
+            itemInterface.onClickItem(holder.adapterPosition)
+        }
     }
+
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var arrowImage: ImageView
         var image: ImageView
         var title: TextView
         var desc: TextView
-
+        var itemCard: CardView
 
 
         init {
-            arrowImage= itemView.findViewById(R.id.arrowImage)
+            arrowImage = itemView.findViewById(R.id.arrowImage)
             image = itemView.findViewById(R.id.image)
             title = itemView.findViewById(R.id.title)
             desc = itemView.findViewById(R.id.desc)
+            itemCard = itemView.findViewById(R.id.item_card)
         }
 
     }
 
-    fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-
-        var data = dataList[position]
-        holder.title.text = data.title
-        holder.desc.text = data.desc
-        holder.image.setImageResource(data.image)
-        holder.arrowImage.setImageResource(data.arrowImage)
-
-
-
-
-    }
     override fun getItemCount() = dataList.size
 
-    fun setItemClickListener(clickListener: View.OnClickListener) {
-        onItemClickListener = clickListener
+    inner class HoabelPromiseViewHolder(var binding: PromisesViewBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    interface PromisedItemInterface {
+        fun onClickItem(position: Int)
     }
 
 }
