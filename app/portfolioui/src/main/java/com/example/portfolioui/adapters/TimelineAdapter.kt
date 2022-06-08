bc21429c9300c71f696fba42a3fa6de2d2e0ce7e
Aftab <aftab.bhadki@ergos.in>
2022-06-08 18:08:48 +0530

    project time line and other issues fixed

--- app/portfolioui/src/main/java/com/example/portfolioui/adapters/TimelineAdapter.kt
+++ app/portfolioui/src/main/java/com/example/portfolioui/adapters/TimelineAdapter.kt
@@ -1,13 +1,18 @@
 package com.example.portfolioui.adapters
 
 import android.content.Context
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.emproto.networklayer.response.portfolio.prtimeline.ProjectTimeline
+import com.example.portfolioui.R
 import com.example.portfolioui.databinding.ItemTimelineDataBinding
 import com.example.portfolioui.databinding.ItemTimelineHeaderBinding
+import com.example.portfolioui.databinding.ItemTimelineStepReraBinding
 import com.example.portfolioui.models.StepsModel
 import com.example.portfolioui.models.TimelineHeaderData
 import com.example.portfolioui.models.TimelineModel
@@ -22,6 +27,7 @@ class TimelineAdapter(
     companion object {
         const val TYPE_HEADER = 0
         const val TYPE_LIST = 1
+        const val TYPE_RERA = 3
         const val TYPE_DISCLAIMER = 2
     }
 
@@ -38,6 +44,15 @@ class TimelineAdapter(
                     false
                 )
                 return HeaderHolder(view)
+            }
+            TYPE_RERA -> {
+                val view = ItemTimelineStepReraBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return StepsReraViewHolder(view)
+
             }
 
             else -> {
@@ -62,25 +77,38 @@ class TimelineAdapter(
                 header_holder.binding.projectName.text = headerData.projectName
                 header_holder.binding.tvAddress.text = headerData.address
             }
+            TYPE_RERA -> {
+                val listData = dataList[position].data as ProjectTimeline
+                val listHolder = holder as StepsReraViewHolder
+
+                listHolder.binding.textView7.text = showHTMLText(
+                    String.format(
+                        context.getString(R.string.tv_receipt),
+                        "View Details"
+                    )
+                )
+
+            }
             TYPE_LIST -> {
                 val listData = dataList[position].data as ProjectTimeline
+                val listHolder = holder as StepsListHolder
 
+                listHolder.binding.textHeader.text = listData.timeLineSectionHeading
                 val stepsList = ArrayList<StepsModel>()
                 for (item in listData.timeLines) {
                     when (item.sections[0].values.percentage) {
-                        0 -> stepsList.add(StepsModel(StepsAdapter.TYPE_INSTART, item))
-                        in 1..99 -> {
+                        0.0 -> stepsList.add(StepsModel(StepsAdapter.TYPE_INSTART, item))
+                        in 1.0..99.99 -> {
                             stepsList.add(StepsModel(StepsAdapter.TYPE_INPROGRESS, item))
                         }
                         else -> stepsList.add(StepsModel(StepsAdapter.TYPE_COMPLETED, item))
                     }
                 }
-                val listHolder = holder as StepsListHolder
-                listHolder.binding.textHeader.text = listData.timeLineSectionHeading
                 listHolder.binding.stepsList.layoutManager = LinearLayoutManager(context)
                 listHolder.binding.stepsList.adapter =
                     StepsAdapter(context, stepsList, null)
             }
+
 
         }
     }
@@ -94,8 +122,19 @@ class TimelineAdapter(
     inner class StepsListHolder(var binding: ItemTimelineDataBinding) :
         RecyclerView.ViewHolder(binding.root)
 
+    inner class StepsReraViewHolder(var binding: ItemTimelineStepReraBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
     interface TimelineInterface {
         fun onClickItem(position: Int)
+    }
+
+    fun showHTMLText(message: String?): Spanned {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Html.fromHtml(message, Html.FROM_HTML_MODE_COMPACT)
+        } else {
+            Html.fromHtml(message)
+        }
     }
 
 }
