3e98af101b347c8a04c41e7af25f10eb61f9c3e0
Aftab <aftab.bhadki@ergos.in>
2022-06-09 15:02:12 +0530

    configured uat env

--- networklayer/src/main/java/com/emproto/networklayer/di/DataModule.kt
+++ networklayer/src/main/java/com/emproto/networklayer/di/DataModule.kt
@@ -55,54 +55,12 @@ class DataModule(private val application: Application) {
         return Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .client(defaultHttpClient)
-            .baseUrl(ApiConstants.BASE_URL_DEV).build()
+            .baseUrl(ApiConstants.BASE_URL_UAT).build()
     }
 
     @Provides
     @Singleton
     fun provideApiService(retrofit: Retrofit): ApiService {
-        return retrofit.create(ApiService::class.java)
-    }
-
-    @Provides
-    @Singleton
-    @Named("dummytoken")
-    fun provideDummy(): Retrofit {
-        val loggingInterceptor = HttpLoggingInterceptor()
-        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-
-//        val httpClient = OkHttpClient.Builder()
-//        httpClient.connectTimeout(1000, TimeUnit.SECONDS)
-//            .readTimeout(100, TimeUnit.SECONDS)
-//            .writeTimeout(100, TimeUnit.SECONDS)
-//        httpClient.addInterceptor(Interceptor {
-//            val request = it.request().newBuilder().addHeader("jwt","").build()
-//            return it.proceed(request)
-//        })
-
-        val defaultHttpClient: OkHttpClient =
-            OkHttpClient.Builder().addInterceptor(
-                Interceptor { chain ->
-                    val request: Request = chain.request().newBuilder()
-                        //.addHeader("jwt", getAppPreference().getToken()).build()
-                        .addHeader(
-                            "jwt",
-                            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjoxLCJlbWFpbCI6Im9ta2FyQGVtcHJvdG8uY29tIiwibGF0ZXN0QWRtaW5Mb2dJZCI6MzAsIm1vZHVsZXMiOltudWxsXSwiaXNzdWVkQXQiOjE2NTE4OTk5NzY5NzUsImV4cGlyZWRBdCI6MTY1MjUwNDc3Njk3NX0sImlhdCI6MTY1MTg5OTk3NiwiZXhwIjoxNjU0NDkxOTc2fQ.Onv30rpwuOk8gxphwljYQd47JZ2M7giMp4qUGXurf0w"
-                        ).build()
-                    chain.proceed(request)
-                }).addInterceptor(loggingInterceptor).build()
-
-
-        return Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(defaultHttpClient)
-            .baseUrl(ApiConstants.BASE_URL_DEV).build()
-    }
-
-    @Provides
-    @Singleton
-    @Named("dummy")
-    fun dummyApiService(@Named("dummytoken") retrofit: Retrofit): ApiService {
         return retrofit.create(ApiService::class.java)
     }
 
--- networklayer/src/main/java/com/emproto/networklayer/response/portfolio/dashboard/GeneralInfoEscalationGraph.kt
+++ networklayer/src/main/java/com/emproto/networklayer/response/portfolio/dashboard/GeneralInfoEscalationGraph.kt
@@ -2,7 +2,7 @@ package com.emproto.networklayer.response.portfolio.dashboard
 
 data class GeneralInfoEscalationGraph(
     val dataPoints: DataPoints,
-    val estimatedAppreciation: Int,
+    val estimatedAppreciation: Double,
     val xAxisDisplayName: String,
     val yAxisDisplayName: String
 )
