fec8263ba1acb651e6489f47f955a83afb68d171
varuntej_d <derangula.varuntej@ergos.in>
2022-06-25 00:32:47 +0530

    null checks in account

--- networklayer/src/main/java/com/emproto/networklayer/feature/ProfileDataSource.kt
+++ networklayer/src/main/java/com/emproto/networklayer/feature/ProfileDataSource.kt
@@ -91,7 +91,7 @@ class ProfileDataSource(val application: Application) : BaseDataSource(applicati
 
     ): Response<UploadDocumentResponse> {
         return apiService.uploadKycDocument(
-            MultipartBody.Part.createFormData("extension", extension!!), MultipartBody.Part.createFormData("file", file.name, RequestBody.create("image/*".toMediaTypeOrNull(), file)), MultipartBody.Part.createFormData("documentCategory", selectedDoc!!)
+            MultipartBody.Part.createFormData("extension", extension!!), MultipartBody.Part.createFormData("file", file.name, RequestBody.create("image/*".toMediaTypeOrNull(), file)), MultipartBody.Part.createFormData("documentType", selectedDoc!!)
         )
     }
 
