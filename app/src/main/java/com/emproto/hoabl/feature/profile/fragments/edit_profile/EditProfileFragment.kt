25ebf70bffe2724526904a31c1b0e2778fa626a1
MohammedTariq97 <tariq@emproto.com>
2022-06-29 12:42:54 +0530

    Solved image rotation issue, arrow mark issue

--- app/src/main/java/com/emproto/hoabl/feature/profile/fragments/edit_profile/EditProfileFragment.kt
+++ app/src/main/java/com/emproto/hoabl/feature/profile/fragments/edit_profile/EditProfileFragment.kt
@@ -13,6 +13,8 @@ import android.content.pm.PackageManager
 import android.database.Cursor
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -852,11 +854,33 @@ class EditProfileFragment : BaseFragment() {
         val selectedImage = cameraFile.path
         destinationFile = cameraFile
         val thumbnail = BitmapFactory.decodeFile(selectedImage)
+        val ei = ExifInterface(cameraFile.path)
+        val orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,ExifInterface.ORIENTATION_UNDEFINED)
+        val rotatedBitmap = when(orientation){
+            ExifInterface.ORIENTATION_ROTATE_90 -> {
+                rotateImage(thumbnail, 90f)
+            }
+            ExifInterface.ORIENTATION_ROTATE_180 -> {
+                rotateImage(thumbnail, 180f)
+            }
+            ExifInterface.ORIENTATION_ROTATE_270 -> {
+                rotateImage(thumbnail, 270f)
+            }
+            ExifInterface.ORIENTATION_NORMAL -> {
+                thumbnail
+            }
+            else -> {
+                thumbnail
+            }
+        }
 
         binding.profileImage.visibility = View.VISIBLE
         binding.profileUserLetters.visibility = View.GONE
-
-        binding.profileImage.setImageBitmap(thumbnail)
+        try{
+            binding.profileImage.setImageBitmap(rotatedBitmap)
+        }catch (e:Exception){
+            e.message
+        }
 
         if ((requireActivity() as BaseActivity).isNetworkAvailable()) {
             callingUploadPicApi(cameraFile)
@@ -869,6 +893,15 @@ class EditProfileFragment : BaseFragment() {
 
             )
         }
+    }
+
+    fun rotateImage(source: Bitmap, angle: Float): Bitmap? {
+        val matrix = Matrix()
+        matrix.postRotate(angle)
+        return Bitmap.createBitmap(
+            source, 0, 0, source.width, source.height,
+            matrix, true
+        )
     }
 
     private fun onSelectFromGalleryResult(data: Intent) {
