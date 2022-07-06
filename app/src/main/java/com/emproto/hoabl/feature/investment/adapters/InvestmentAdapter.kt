9646334c49907f85d357cd00420122c30f41dd49
varuntej_d <derangula.varuntej@ergos.in>
2022-07-06 11:39:32 +0530

    null check in-SimilarInvestment

--- app/src/main/java/com/emproto/hoabl/feature/investment/adapters/InvestmentAdapter.kt
+++ app/src/main/java/com/emproto/hoabl/feature/investment/adapters/InvestmentAdapter.kt
@@ -31,10 +31,10 @@ class InvestmentAdapter(
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val element = list[position]
         holder.binding.apply {
-            tvNoViews.text = Utility.coolFormat(element.fomoContent.noOfViews.toDouble(),0)
+            tvNoViews.text = Utility.coolFormat(element.fomoContent?.noOfViews!!.toDouble(),0)
             tvItemLocationName.text = element.launchName
-            tvItemLocation.text = "${element.address.city}, ${element.address.state}"
-            val amount = element.priceStartingFrom.toDouble() / 100000
+            tvItemLocation.text = "${element.address?.city}, ${element.address?.state}"
+            val amount = element.priceStartingFrom!!.toDouble() / 100000
             val convertedAmount = amount.toString().replace(".0","")
             tvItemAmount.text = SpannableStringBuilder()
                 .bold { append("₹${convertedAmount} L") }
@@ -45,7 +45,7 @@ class InvestmentAdapter(
             tvItemLocationInfo.text = element.shortDescription
             Glide
                 .with(context)
-                .load(element.projectCoverImages.newInvestmentPageMedia.value.url)
+                .load(element.projectCoverImages?.newInvestmentPageMedia?.value?.url)
                 .into(ivItemImage)
 
             cvTopView.setOnClickListener {
