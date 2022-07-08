07633cca9667dd6117a4f0c9c60427d31a802bde
varuntej_d <derangula.varuntej@ergos.in>
2022-07-08 13:06:32 +0530

    profile

--- app/src/main/java/com/emproto/hoabl/feature/profile/fragments/edit_profile/EditProfileFragment.kt
+++ app/src/main/java/com/emproto/hoabl/feature/profile/fragments/edit_profile/EditProfileFragment.kt
@@ -1075,7 +1075,6 @@ class EditProfileFragment : BaseFragment() {
     ) { result ->
         if (result.resultCode === Activity.RESULT_OK) {
             onCaptureImageResult()
-
         }
     }
 
