bc3ce2da76901043b9f81ebbbfe761a826d50cb9
Aftab <aftab.bhadki@ergos.in>
2022-07-12 15:23:36 +0530

    final commit

--- app/src/main/java/com/emproto/hoabl/HoablApplication.kt
+++ app/src/main/java/com/emproto/hoabl/HoablApplication.kt
@@ -2,8 +2,15 @@ package com.emproto.hoabl
 
 import android.app.Application
 import com.emproto.hoabl.di.*
+import com.mixpanel.android.mpmetrics.MixpanelAPI
 
 class HoablApplication : Application(), HomeComponentProvider {
+
+    override fun onCreate() {
+        super.onCreate()
+        val mixPanel = MixpanelAPI.getInstance(this, "b6e5cdf30ddcdfb3886bdaa58a1d0acc")
+        mixPanel.track("Login")
+    }
 
     override fun homeComponent(): HomeComponent {
         return DaggerHomeComponent.builder()
