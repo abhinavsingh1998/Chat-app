d0187f136a08d077a4d0449735f1624b665e502a
Abhinav <abhinavp@emproto.com>
2022-08-19 13:04:01 +0530

    Home Portfolio card issue fixed

--- networklayer/src/main/java/com/emproto/networklayer/response/home/PortFolioData.kt
+++ networklayer/src/main/java/com/emproto/networklayer/response/home/PortFolioData.kt
@@ -2,7 +2,7 @@ package com.emproto.networklayer.response.home
 
 data class PortFolioData(
     val amountInvested: Double,
-    val amountPending: Int,
+    val amountPending: Double,
     val investmentCount: Int,
-    val totalAreaSqFt: Int
+    val totalAreaSqFt: Double
 )
