58461999a55a3b0a323d8f5434674da043144972
varuntej_d <derangula.varuntej@ergos.in>
2022-09-13 11:24:53 +0530

    Removing warnings

--- app/src/main/java/com/emproto/hoabl/feature/profile/fragments/feedback/FeedbackFragment.kt
+++ app/src/main/java/com/emproto/hoabl/feature/profile/fragments/feedback/FeedbackFragment.kt
@@ -9,11 +9,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.isVisible
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.emproto.core.BaseFragment
+import com.emproto.core.Constants
 import com.emproto.hoabl.R
 import com.emproto.hoabl.databinding.FragmentFeedbackBinding
 import com.emproto.hoabl.databinding.FragmentFeedbackBinding.*
@@ -22,31 +23,29 @@ import com.emproto.hoabl.feature.home.views.HomeActivity
 import com.emproto.hoabl.viewmodels.ProfileViewModel
 import com.emproto.hoabl.viewmodels.factory.ProfileFactory
 import com.emproto.networklayer.request.profile.FeedBackRequest
-import com.emproto.networklayer.response.BaseResponse
 import com.emproto.networklayer.response.enums.Status
-import com.emproto.networklayer.response.profile.FeedBackResponse
 import javax.inject.Inject
 
 
 class FeedbackFragment : BaseFragment(), View.OnClickListener {
 
-    var lightface: Typeface? = null
-    var boldface: Typeface? = null
+    private var lightface: Typeface? = null
+    private var boldface: Typeface? = null
 
     @Inject
     lateinit var factory: ProfileFactory
     lateinit var profileViewModel: ProfileViewModel
-    lateinit var feedBackRequest: FeedBackRequest
+    private lateinit var feedBackRequest: FeedBackRequest
     lateinit var description: String
 
-    var ratings = 0
+    private var ratings = 0
     lateinit var binding: FragmentFeedbackBinding
     val bundle = Bundle()
     var list = ArrayList<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = inflate(inflater, container, false)
         (requireActivity() as HomeActivity).hideBottomNavigation()
         (requireActivity().application as HomeComponentProvider).homeComponent().inject(this)
@@ -73,18 +72,19 @@ class FeedbackFragment : BaseFragment(), View.OnClickListener {
                 description = p0.toString()
                 if (p0.toString().length == 250) {
                     binding.editIssuesLayout.boxStrokeColor =
-                        resources.getColor(R.color.text_red_color)
+                        ContextCompat.getColor(context!!,R.color.text_red_color)
                     binding.txtcount.isVisible = true
                     binding.maxDesc.isVisible = false
-                    binding.experienceTv.setTextColor(resources.getColorStateList(R.color.text_red_color))
+                    binding.experienceTv.setTextColor(ContextCompat.getColorStateList(context!!,R.color.text_red_color))
                 } else {
-                    binding.editIssuesLayout.boxStrokeColor = resources.getColor(R.color.app_color)
+                    binding.editIssuesLayout.boxStrokeColor =
+                        context?.let { ContextCompat.getColor(it,R.color.app_color) }!!
                     binding.txtcount.isVisible = false
                     binding.maxDesc.isVisible = true
-                    "${description.length}/250"+" Characters".also { binding.maxDesc.text = it }
-                    binding.experienceTv.setTextColor(resources.getColorStateList(R.color.text_color))
+                    "${description.length}/250  Characters".also { binding.maxDesc.text = it }
+                    with(binding) { experienceTv.setTextColor(ContextCompat.getColorStateList(
+                        context!!,R.color.text_color)) }
                 }
-
             }
 
             override fun afterTextChanged(p0: Editable?) {
@@ -118,93 +118,92 @@ class FeedbackFragment : BaseFragment(), View.OnClickListener {
     }
 
     private fun selected5() {
-        binding.ivRating5.setImageDrawable(resources.getDrawable(R.drawable.selected5))
+        binding.ivRating5.setImageDrawable(context?.let { ContextCompat.getDrawable(it,R.drawable.selected5) })
         selectedFontAndColor(binding.excelentTxt)
     }
 
     private fun selected4() {
-        binding.ivRating4.setImageDrawable(resources.getDrawable(R.drawable.selected4))
+        binding.ivRating4.setImageDrawable(context?.let { ContextCompat.getDrawable(it,R.drawable.selected4) })
         selectedFontAndColor(binding.goodTxt)
     }
 
     private fun selected2() {
-        binding.ivRating2.setImageDrawable(resources.getDrawable(R.drawable.selected2))
+        binding.ivRating2.setImageDrawable(context?.let { ContextCompat.getDrawable(it,R.drawable.selected2) })
         selectedFontAndColor(binding.badTxt)
     }
 
     private fun selected3() {
-        binding.ivRating3.setImageDrawable(resources.getDrawable(R.drawable.selected3))
+        binding.ivRating3.setImageDrawable(context?.let { ContextCompat.getDrawable(it,R.drawable.selected3) })
         selectedFontAndColor(binding.okTxt)
     }
 
     private fun selected1() {
-        binding.ivRating1.setImageDrawable(resources.getDrawable(R.drawable.selected1))
+        binding.ivRating1.setImageDrawable(context?.let { ContextCompat.getDrawable(it,R.drawable.selected1) })
         selectedFontAndColor(binding.veryPoorTxt)
     }
 
     private fun selectedFontAndColor(selectedText: TextView) {
-        selectedText.setTextColor(resources.getColor(R.color.black))
+        selectedText.setTextColor(ContextCompat.getColor(requireContext(),R.color.black))
         selectedText.typeface = boldface
     }
 
     private fun unselected5() {
-        binding.ivRating5.setImageDrawable(resources.getDrawable(R.drawable.unselected5))
+        binding.ivRating5.setImageDrawable(context?.let { ContextCompat.getDrawable(it,R.drawable.unselected5) })
         unSelectedFontAndColor(binding.excelentTxt)
     }
 
     private fun unselected4() {
-        binding.ivRating4.setImageDrawable(resources.getDrawable(R.drawable.unselected4))
+        binding.ivRating4.setImageDrawable(context?.let { ContextCompat.getDrawable(it,R.drawable.unselected4) })
         unSelectedFontAndColor(binding.goodTxt)
     }
 
     private fun unselected3() {
-        binding.ivRating3.setImageDrawable(resources.getDrawable(R.drawable.unselected3))
+        binding.ivRating3.setImageDrawable(context?.let { ContextCompat.getDrawable(it,R.drawable.unselected3) })
         unSelectedFontAndColor(binding.okTxt)
     }
 
     private fun unselected2() {
-        binding.ivRating2.setImageDrawable(resources.getDrawable(R.drawable.unselected2))
+        binding.ivRating2.setImageDrawable(context?.let { ContextCompat.getDrawable(it,R.drawable.unselected2) })
         unSelectedFontAndColor(binding.badTxt)
     }
 
     private fun unselected1() {
-        binding.ivRating1.setImageDrawable(resources.getDrawable(R.drawable.unselected1))
+        binding.ivRating1.setImageDrawable(context?.let { ContextCompat.getDrawable(it,R.drawable.unselected1) })
         unSelectedFontAndColor(binding.veryPoorTxt)
     }
 
     private fun unSelectedFontAndColor(unSelectedText: TextView) {
-        unSelectedText.setTextColor(resources.getColor(R.color.category_location_ash_color))
+        unSelectedText.setTextColor(ContextCompat.getColor(requireContext(),R.color.category_location_ash_color))
         unSelectedText.typeface = lightface
     }
 
     private fun initObserver() {
         profileViewModel.submitFeedback(feedBackRequest)
-            .observe(viewLifecycleOwner, object : Observer<BaseResponse<FeedBackResponse>> {
-                override fun onChanged(it: BaseResponse<FeedBackResponse>?) {
-                    when (it!!.status) {
-                        Status.ERROR -> {
-                            binding.progressBar.isVisible = false
-                            binding.shareYourFeedback.isVisible = true
-                            binding.shareYourFeedback.text = "Share your feedback"
-                            (requireActivity() as HomeActivity).showErrorToast(
-                                it.message!!
-                            )
-                        }
-                        Status.SUCCESS -> {
-                            binding.progressBar.isVisible = false
-                            binding.shareYourFeedback.isVisible = true
-                            val dialog = FeedBackSubmittedPopup()
-                            dialog.isCancelable = false
-                            dialog.show(childFragmentManager, "submitted")
+            .observe(viewLifecycleOwner
+            ) {
+                when (it!!.status) {
+                    Status.ERROR -> {
+                        binding.progressBar.isVisible = false
+                        binding.shareYourFeedback.isVisible = true
+                        binding.shareYourFeedback.text = requireContext().getString(R.string.share_your_feedback)
+                        (requireActivity() as HomeActivity).showErrorToast(
+                            it.message!!
+                        )
+                    }
+                    Status.SUCCESS -> {
+                        binding.progressBar.isVisible = false
+                        binding.shareYourFeedback.isVisible = true
+                        val dialog = FeedBackSubmittedPopup()
+                        dialog.isCancelable = false
+                        dialog.show(childFragmentManager, "submitted")
 
-                        }
-                        Status.LOADING -> {
-                            binding.shareYourFeedback.isVisible = false
-                            binding.progressBar.isVisible = true
-                        }
+                    }
+                    Status.LOADING -> {
+                        binding.shareYourFeedback.isVisible = false
+                        binding.progressBar.isVisible = true
                     }
                 }
-            })
+            }
     }
 
     override fun onClick(view: View?) {
@@ -216,45 +215,45 @@ class FeedbackFragment : BaseFragment(), View.OnClickListener {
             }
             R.id.home_checkbox -> {
                 if (binding.homeCheckbox.isChecked) {
-                    list.add("5001")
+                    list.add(Constants.FIVE_THOUSAND_ONE)
                 } else {
-                    list.remove("5001")
+                    list.remove(Constants.FIVE_THOUSAND_ONE)
                 }
             }
             R.id.checkbox_invest -> {
                 if (binding.checkboxInvest.isChecked) {
-                    list.add("5002")
+                    list.add(Constants.FIVE_THOUSAND_TWO)
                 } else {
-                    list.remove("5002")
+                    list.remove(Constants.FIVE_THOUSAND_TWO)
                 }
             }
             R.id.checkbox_portfolio -> {
                 if (binding.checkboxPortfolio.isChecked) {
-                    list.add("5003")
+                    list.add(Constants.FIVE_THOUSAND_THREE)
                 } else {
-                    list.remove("5003")
+                    list.remove(Constants.FIVE_THOUSAND_THREE)
                 }
             }
             R.id.checkbox_promises -> {
                 if (binding.checkboxPromises.isChecked) {
-                    list.add("5004")
+                    list.add(Constants.FIVE_THOUSAND_FOUR)
                 } else {
-                    list.remove("5004")
+                    list.remove(Constants.FIVE_THOUSAND_FOUR)
                 }
             }
             R.id.checkbox_profile -> {
                 if (binding.checkboxProfile.isChecked) {
-                    list.add("5005")
+                    list.add(Constants.FIVE_THOUSAND_FIVE)
 
                 } else {
-                    list.remove("5005")
+                    list.remove(Constants.FIVE_THOUSAND_FIVE)
                 }
             }
             R.id.checkbox_other -> {
                 if (binding.checkboxOther.isChecked) {
-                    list.add("5006")
+                    list.add(Constants.FIVE_THOUSAND_SIX)
                 } else {
-                    list.remove("5006")
+                    list.remove(Constants.FIVE_THOUSAND_SIX)
                 }
             }
 
@@ -306,7 +305,7 @@ class FeedbackFragment : BaseFragment(), View.OnClickListener {
                     initObserver()
                 } else {
                     (requireActivity() as HomeActivity).showErrorToast(
-                        "Fill atleast one field"
+                        requireContext().getString(R.string.fill_alteast_one_field)
                     )
                 }
 
