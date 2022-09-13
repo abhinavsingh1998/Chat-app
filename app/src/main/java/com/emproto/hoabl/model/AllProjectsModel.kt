3691baa600f311c35a948aff17191cb26965806a
varuntej_d <derangula.varuntej@ergos.in>
2022-09-13 11:24:53 +0530

    Removing warnings

--- app/src/main/java/com/emproto/hoabl/model/AllProjectsModel.kt
+++
@@ -1,8 +0,0 @@
-package com.emproto.hoabl.model
-
-import com.emproto.networklayer.response.investment.ApData
-
-data class AllProjectsModel(
-    val list :List<ApData>,
-    val isClicked:Boolean
-)
