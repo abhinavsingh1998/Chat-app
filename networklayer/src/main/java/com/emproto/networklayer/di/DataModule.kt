9b31b52267d64cce06a83e59a9937c612c900164
Aftab <aftab.bhadki@ergos.in>
2022-11-11 15:20:53 +0530

    timeout added

--- networklayer/src/main/java/com/emproto/networklayer/di/DataModule.kt
+++ networklayer/src/main/java/com/emproto/networklayer/di/DataModule.kt
@@ -1,7 +1,6 @@
 package com.emproto.networklayer.di
 
 import android.app.Application
-import com.emproto.networklayer.ApiConstants
 import com.emproto.networklayer.ApiService
 import com.emproto.networklayer.BuildConfig
 import com.emproto.networklayer.preferences.AppPreference
@@ -9,15 +8,17 @@ import com.emproto.networklayer.preferences.AppPreferenceImp
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import okhttp3.*
+import okhttp3.CipherSuite.Companion.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256
+import okhttp3.CipherSuite.Companion.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
+import okhttp3.CipherSuite.Companion.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 import java.util.concurrent.TimeUnit
-import javax.inject.Named
 import javax.inject.Singleton
+
 
 @Module
 class DataModule(private val application: Application) {
@@ -34,20 +35,36 @@ class DataModule(private val application: Application) {
         val loggingInterceptor = HttpLoggingInterceptor()
         loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
 
+        val spec: ConnectionSpec = ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+            .tlsVersions(TlsVersion.TLS_1_2)
+            .cipherSuites(
+                TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+                TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                TLS_DHE_RSA_WITH_AES_128_GCM_SHA256
+            )
+            .build()
+
         val defaultHttpClient: OkHttpClient =
-            OkHttpClient.Builder().addInterceptor(
+            OkHttpClient.Builder().connectionSpecs(Collections.singletonList(spec)).addInterceptor(
                 Interceptor { chain ->
                     val request: Request = chain.request().newBuilder()
                         .addHeader("jwt", getAppPreference().getToken()).addHeader("apptype", "app")
                         .build()
                     chain.proceed(request)
-                }).addInterceptor(loggingInterceptor).callTimeout(60,TimeUnit.SECONDS).connectTimeout(60,TimeUnit.SECONDS)
-                .writeTimeout(60,TimeUnit.SECONDS).readTimeout(60,TimeUnit.SECONDS)
+                }).addInterceptor(loggingInterceptor)
+                .callTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
                 .build()
 
 
         return Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().serializeNulls().create()))
+            .addConverterFactory(
+                GsonConverterFactory.create(
+                    GsonBuilder().serializeNulls().create()
+                )
+            )
             .client(defaultHttpClient)
             .baseUrl(BuildConfig.BASE_URL).build()
     }
