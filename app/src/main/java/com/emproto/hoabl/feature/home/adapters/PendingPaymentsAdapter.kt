93418084955ce9e6024ef7456d74943c07b8f3fd
emproto-icf <lalitha@emproto.com>
2022-11-21 16:24:36 +0530

    final changes

--- app/src/main/java/com/emproto/hoabl/feature/home/adapters/PendingPaymentsAdapter.kt
+++ app/src/main/java/com/emproto/hoabl/feature/home/adapters/PendingPaymentsAdapter.kt
@@ -1,11 +1,19 @@
 package com.emproto.hoabl.feature.home.adapters
 
 import android.content.Context
+import android.graphics.Color
+import android.text.SpannableStringBuilder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.bold
+import androidx.core.text.color
 import androidx.recyclerview.widget.RecyclerView
+import com.emproto.core.Constants
+import com.emproto.hoabl.R
 import com.emproto.hoabl.databinding.CompletePaymentCardBinding
+import com.emproto.hoabl.utils.Extensions.makeClickable
+import com.emproto.hoabl.utils.Extensions.makeLinks
 import com.emproto.hoabl.utils.ItemClickListener
 import com.emproto.networklayer.response.actionItem.Data
 
@@ -28,15 +36,30 @@ class PendingPaymentsAdapter(
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val item = list!![holder.adapterPosition]
 
+        holder.binding.kycCard.setOnClickListener(View.OnClickListener {
+            itemIntrface.onItemClicked(it, position, holder.itemId.toString())
+        })
+
         holder.binding.apply {
             actionRequired.text = item!!.cardTitle
 
             pendingKyc.text = item!!.displayTitle
-            uploadKycStatement.text = item!!.displayText
+
+            uploadKycStatement.text = SpannableStringBuilder()
+                .append(item!!.displayText+" ")
+                .color(Color.parseColor("#676ac0")){bold{append(Constants.CLICKTOKNOWMORE)}
+                }
+
+            uploadKycStatement.makeClickable(
+                Pair("Click to know more", View.OnClickListener {
+                    itemIntrface.onItemClicked(it, position, holder.itemId.toString())
+                }),
+            )
 
             seeAllPendingPayment.setOnClickListener(View.OnClickListener {
                 itemIntrface.onItemClicked(it, position, holder.itemId.toString())
             })
+
 
         }
 
